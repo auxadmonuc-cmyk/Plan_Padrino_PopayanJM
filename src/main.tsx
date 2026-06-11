9f44920e3eb86616108e4fc4f57594d04d19e809
auxadmonuc-cmyk <auxadmon.uc@gmail.com>
2026-06-11 10:16:00 -0500

    main bug del traductor

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,46 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Monkeypatch Node.prototype to prevent Google Translate from crashing React
+if (typeof window !== 'undefined') {
+  if (typeof Node === 'function' && Node.prototype) {
+    const originalRemoveChild = Node.prototype.removeChild;
+    Node.prototype.removeChild = function <T extends Node>(child: T): T {
+      if (child.parentNode !== this) {
+        if (typeof console !== 'undefined' && console.error) {
+          console.error('removeChild: Element is not a child of this node', {
+            child,
+            parent: this,
+          });
+        }
+        return child;
+      }
+      return originalRemoveChild.call(this, child) as T;
+    };
+
+    const originalInsertBefore = Node.prototype.insertBefore;
+    Node.prototype.insertBefore = function <T extends Node>(
+      newNode: T,
+      referenceNode: Node | null
+    ): T {
+      if (referenceNode && referenceNode.parentNode !== this) {
+        if (typeof console !== 'undefined' && console.error) {
+          console.error('insertBefore: Reference element is not a child of this node', {
+            newNode,
+            referenceNode,
+            parent: this,
+          });
+        }
+        return newNode;
+      }
+      return originalInsertBefore.call(this, newNode, referenceNode) as T;
+    };
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
